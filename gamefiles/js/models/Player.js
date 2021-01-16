a5e7b706be0348238f1f940ca35a848e4c1ae434
davq23 <davidquinterogranadillo@gmail.com>
2021-01-15 23:20:38 -0400

    Fixed logic error

--- gamefiles/js/models/Player.js
+++ gamefiles/js/models/Player.js
@@ -35,7 +35,7 @@ export class Player {
                 this.positions[i] = this.positions[i - 1];
             }
 
-            while(this.positions[0].x != head.x && this.positions[0].y != head.y) {
+            while(this.positions[0].x != head.x || this.positions[0].y != head.y) {
                 switch (this.direction) {
                     case playerDown:
                         this.positions[0].y += 20;
