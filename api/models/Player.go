c7e7b929eb7a88d6073046fb33a01b07d01f2990
davq23 <davidquinterogranadillo@gmail.com>
2021-01-18 01:26:31 -0400

    Increased closeness check by 100

--- api/models/Player.go
+++ api/models/Player.go
@@ -76,7 +76,7 @@ func (p *Player) Move() {
 
 // CheckCollision checks whether the head of another player has collided with the current player and also at which position
 func (p *Player) CheckCollision(player *Player) (bool, int) {
-	if distance(p.Positions[0], player.Positions[0]) <= p.PlayerTotalLength+distanceAdjustment {
+	if distance(p.Positions[0], player.Positions[0]) <= p.PlayerTotalLength+distanceAdjustment+100 {
 		for i := 0; i < p.PlayerLength; i++ {
 			dist := distance(p.Positions[i], player.Positions[0])
 
