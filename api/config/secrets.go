06b456408f6aebb38cdc310e3303550c0c59deb8
davq23 <davidquinterogranadillo@gmail.com>
2021-08-17 07:08:03 -0400

    Put .environment secret

--- api/config/secrets.go
+++ api/config/secrets.go
@@ -1,4 +1,4 @@
 package config
 
 // JWTSecret is the secret used to verify the JWT token
-const JWTSecret = "dLWFOrq3c7y0I3Ka0MvgnRVUyG3rvI7RRHlzLb85FefEnX9YVutcpQE_rekPUQ5K5BR0awiLdddgpZW_1j5wqa3KQLtHDpiESuTlmWkVebQbxIaP21c4GA%3D%3D"
+const JWTSecret = os.Getenv("jwtSecret")
